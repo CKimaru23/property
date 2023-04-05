fca9287217c35e0f1180d43ecb0c08a36f8aa59a
Collins Kiragu <collins.kiragu@student.moringaschool.com>
2023-04-06 02:21:05 +0300

    Apartment model done

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,2 +1,6 @@
 class Apartment < ApplicationRecord
+    belongs_to :landlord
+    validates :name, presence: true, length: { maximum: 50 }
+    validates :image, presence: true
+    validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
