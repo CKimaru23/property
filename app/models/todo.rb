a550cc42c6caa27e28cba1c71fa95f3ffa4c9f20
Collins Kiragu <collins.kiragu@student.moringaschool.com>
2023-04-06 15:10:31 +0300

    Todo Model Done

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,10 @@
 class Todo < ApplicationRecord
   belongs_to :landlord
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :category, presence: true
+  validates :priority, inclusion: { in: %w(low medium high) }
+  validates :due_date, presence: true, numericality: {greater_than_or_equal_to:2023, less_than_or_equal_to:Date.today.year}
+  validates :completed, presence: true
 end
